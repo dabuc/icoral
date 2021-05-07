0371c2e4e5bec28f21e29dbde3b1b290bb36717b
dabuc <7982737+dabuc@users.noreply.github.com>
2021-05-07 12:26:11 +0800

    添加quant项目

--- icoral/__init__.py
+++ icoral/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
+import flask
 from icoral.blueprints.home import home_bp
 
+
 def create_app(config_name=None):
-    app=Flask(__name__)
+    app = Flask(__name__)
     app.register_blueprint(home_bp)
 
     return app
-
-
